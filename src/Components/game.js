ed2cb9f6bdc0caf6695fc250df7cced67db68c58
emagdnimMQT <mark.w.novak@gmail.com>
2021-06-18 13:04:56 -0400

    still working on game logic.

--- src/Components/game.js
+++ src/Components/game.js
@@ -1,21 +1,26 @@
 import React, { useState } from 'react';
 
 export default function Game(props) {
-    const [clickCount, setClickCount] = useState(0);
     const [round, setRound] = useState(1);
     const [target, setTarget] = useState();
+    const [humanGuess, sethumanGuess] = useState(0);
     const [computerGuess, setComputerGuess] = useState();
+    const [humanScore, setHumanScore] = useState(0);
+    const [computerScore, setComputerScore] = useState(0);
 
     const handlePlus = () => {
-        setClickCount((prevClickCount) => prevClickCount + 1)
+        sethumanGuess((prevHumanGuess) => prevHumanGuess + 1)
     }
 
     const handleMinus = () => {
-        setClickCount((prevClickCount) => prevClickCount - 1)
+        sethumanGuess((prevHumanGuess) => prevHumanGuess - 1)
     }
 
     const roundNumber = () => {
-        setRound((prevRound) => prevRound + 1)
+        setRound((prevRound) => prevRound + 1);
+        sethumanGuess(0);
+        setTarget();
+        setComputerGuess();
     }
 
     const generateTarget = () => {
@@ -31,18 +36,54 @@ export default function Game(props) {
         generateComputerGuess();
     }
 
+    function handleRetry() {
+        sethumanGuess(0);
+        setRound(1);
+        setTarget();
+        setComputerGuess();
+        setHumanScore(0);
+        setComputerScore(0);
+    }
+
+   const compareGuesses = (humanGuess, computerGuess, target) => {
+       const humanDifference = Math.abs(target - humanGuess);
+       const computerDifference = Math.abs(target - computerGuess);
+       if(humanDifference > computerDifference) {
+           return setComputerScore((prevComputerScore) => prevComputerScore + 1);
+       } else if (computerDifference > humanDifference) {
+           return setHumanScore((prevHumanScore) => prevHumanScore + 1);
+       }
+       
+   }
+
+   /*const humanIsWinner = compareGuesses(humanGuess, computerGuess, target);
+   const winner = humanIsWinner ? 'human' : 'computer';
+
+   const updateScore = winner => {
+       if(winner === 'human') {
+           setHumanScore((prevHumanScore) => prevHumanScore + 1);
+       } else if (winner === 'computer') {
+           setComputerScore((prevComputerScore) => prevComputerScore + 1);
+       }
+   }*/
+
+   function compare() {
+       compareGuesses(humanGuess, computerGuess, target);
+   }
+
+   
 
   return (
     <div className="Guesing-area">
         <div className="rounds">
-          <p className="Round-label">Round: <span id="round-number">{round}</span></p>
-          <p className="guess-label">Taret Number: <span id="target-number">{target}</span></p>
+          <p className="Round-label">Round: {round}</p>
+          <p className="guess-label">Taret Number: {target}</p>
         </div>
 
         <div className="guess computer-guess">
             <div className="guess-title">
               <p className="guess-label">Computer</p>
-              <p className="score-labe">Score: <span id="computer-score">0</span></p>
+              <p className="score-labe">Score: {computerScore}</p>
             </div>
             <p id="computer-guess">Computer Guess: {computerGuess}</p>
             <p className="winning-text" id="computer-wins"></p>
@@ -52,28 +93,29 @@ export default function Game(props) {
         <div className="guess human-guess">
             <div className="guess-title">
               <p className="guess-label">You</p>
-              <p className="score-label">Score: <span id="human-score">0</span></p>
+              <p className="score-label">Score: {humanScore}</p>
             </div>
             <div>
-                <input id="human-guess" value={clickCount} />
+                <input id="human-guess" value={humanGuess} />
             </div>
             <div className="number-controls">
                 <button
                 className="number-control left" 
                 id="subtract" 
                 onClick={handlePlus} 
-                disabled={clickCount > 9}>+</button>
+                disabled={humanGuess > 9}>+</button>
                 <button 
                 className="number-control right" 
                 id="add" 
                 onClick={handleMinus} 
-                disabled={clickCount <= 0}>-</button>
+                disabled={humanGuess <= 0}>-</button>
             </div>
-            <button className="button" id="guess" onClick={randomGenerator}>Make a Guess!</button>
+            <button className="button" id="guess" onClick={randomGenerator} onChange={compare}>Make a Guess!</button>
         </div>
 
-        <div className="next-round-container" onClick={roundNumber}>
-            <button className="button" id="next-round" >Next Round</button>
+        <div className="next-round-container" >
+            <button className="button" id="next-round" onClick={roundNumber}>Next Round</button>
+            <button className="button" id="next-round" onClick={handleRetry}>Start Over</button>
         </div>
     </div>
   );
