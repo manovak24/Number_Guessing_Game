ef956994d1879de0062a9e847aaa0787d27411fe
emagdnimMQT <mark.w.novak@gmail.com>
2021-06-18 13:22:24 -0400

    still working on logic.

--- src/Components/game.js
+++ src/Components/game.js
@@ -2,9 +2,9 @@ import React, { useState } from 'react';
 
 export default function Game(props) {
     const [round, setRound] = useState(1);
-    const [target, setTarget] = useState();
+    const [target, setTarget] = useState('?');
     const [humanGuess, sethumanGuess] = useState(0);
-    const [computerGuess, setComputerGuess] = useState();
+    const [computerGuess, setComputerGuess] = useState('?');
     const [humanScore, setHumanScore] = useState(0);
     const [computerScore, setComputerScore] = useState(0);
 
@@ -19,8 +19,8 @@ export default function Game(props) {
     const roundNumber = () => {
         setRound((prevRound) => prevRound + 1);
         sethumanGuess(0);
-        setTarget();
-        setComputerGuess();
+        setTarget('?');
+        setComputerGuess('?');
     }
 
     const generateTarget = () => {
@@ -31,16 +31,11 @@ export default function Game(props) {
         setComputerGuess(Math.floor(Math.random() * 10))
     }
 
-    function randomGenerator() {
-        generateTarget();
-        generateComputerGuess();
-    }
-
     function handleRetry() {
         sethumanGuess(0);
         setRound(1);
-        setTarget();
-        setComputerGuess();
+        setTarget('?');
+        setComputerGuess('?');
         setHumanScore(0);
         setComputerScore(0);
     }
@@ -48,9 +43,9 @@ export default function Game(props) {
    const compareGuesses = (humanGuess, computerGuess, target) => {
        const humanDifference = Math.abs(target - humanGuess);
        const computerDifference = Math.abs(target - computerGuess);
-       if(humanDifference > computerDifference) {
+       if(computerDifference > humanDifference) {
            return setComputerScore((prevComputerScore) => prevComputerScore + 1);
-       } else if (computerDifference > humanDifference) {
+       } else if (humanDifference > computerDifference) {
            return setHumanScore((prevHumanScore) => prevHumanScore + 1);
        }
        
@@ -67,7 +62,9 @@ export default function Game(props) {
        }
    }*/
 
-   function compare() {
+   function runGame() {
+       generateTarget();
+       generateComputerGuess();
        compareGuesses(humanGuess, computerGuess, target);
    }
 
@@ -110,7 +107,7 @@ export default function Game(props) {
                 onClick={handleMinus} 
                 disabled={humanGuess <= 0}>-</button>
             </div>
-            <button className="button" id="guess" onClick={randomGenerator} onChange={compare}>Make a Guess!</button>
+            <button className="button" id="guess" onClick={runGame}>Make a Guess!</button>
         </div>
 
         <div className="next-round-container" >
