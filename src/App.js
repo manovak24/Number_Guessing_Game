01bb5df6072492b19de210ff2e3934b173497450
emagdnimMQT <mark.w.novak@gmail.com>
2021-06-23 12:46:10 -0400

    adding styling to game.

--- src/App.js
+++ src/App.js
@@ -6,28 +6,31 @@ class App extends React.Component {
   
   render() {
     return (
-      <div className="game-container">
-        <header>
-          <h1>Number Guessing Game</h1>
-        </header>
+      <div className="body">
 
-        <Game />
+        <div className="game-container">
+          <header>
+            <h1>Number Guessing Game</h1>
+          </header>
 
-        <div className="instructions">
-          <div className="instruction">
-            <h3>Step 1</h3>
-            <p>Input a number between 0 and 9</p>
-          </div>
-          <div className="instruction">
-            <h3>Step 2</h3>
-            <p>Click "Make a Guess" to submit your guess and see who won the round.</p>
-          </div>
-          <div className="instruction">
-            <h3>Step 3</h3>
-            <p>Click "Next Round" to play again.</p>
+          <Game />
+
+          <div className="instructions">
+            <div className="instruction">
+              <h3>Step 1</h3>
+              <p>Input a number between 0 and 9</p>
+            </div>
+            <div className="instruction">
+              <h3>Step 2</h3>
+              <p>Click "Make a Guess" to submit your guess and see who won the round.</p>
+            </div>
+            <div className="instruction">
+              <h3>Step 3</h3>
+              <p>Click "Next Round" to play again.</p>
+            </div>
           </div>
         </div>
-        
+
       </div>
     )
   }
