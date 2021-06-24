b63a59e67827b30ec88197a3fae68974703e05c0
emagdnimMQT <mark.w.novak@gmail.com>
2021-06-24 17:23:40 -0400

    added google font and fixed style issue in app css.

--- src/App.js
+++ src/App.js
@@ -7,34 +7,33 @@ class App extends React.Component {
   render() {
     return (
       <div className="body">
+              
+        <h1 className="title">Number Guessing Game</h1>
 
         <div className="game-container">
-          <header>
-            <h1 className="title">Number Guessing Game</h1>
-          </header>
-
+          
           <Game />
           
         </div>
 
-          <div className="instructions">
-            <div className="instruction">
-              <h3>Step 1</h3>
-              <p>Select a number between 1 and 10</p>
-            </div>
-            <div className="instruction">
-              <h3>Step 2</h3>
-              <p>Click "Make a Guess" to submit your guess</p>
-            </div>
-            <div className="instruction">
-              <h3>Step 3</h3>
-              <p>Click "Compare!" to determine the winner</p>
-            </div>
-            <div className="instruction">
-              <h3>Step 4</h3>
-              <p>Click "Next Round" to advance to next round</p>
-            </div>
+        <div className="instructions">
+          <div className="instruction">
+            <h3>Step 1</h3>
+            <p>Select a number between 1 and 10</p>
           </div>
+          <div className="instruction">
+            <h3>Step 2</h3>
+            <p>Click "Make a Guess" to submit your guess</p>
+          </div>
+          <div className="instruction">
+            <h3>Step 3</h3>
+            <p>Click "Compare!" to determine the winner</p>
+          </div>
+          <div className="instruction">
+            <h3>Step 4</h3>
+            <p>Click "Next Round" to advance to next round</p>
+          </div>
+        </div>
 
       </div>
     )
